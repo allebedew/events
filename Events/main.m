71562ba98f3557ea90a207330a81cc2295e7ba9e
Aleksey Lebedev <allebedew@yandex-team.ru>
2014-03-23 17:53:53 +0200

    initial commit

---
+++ Events/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Events
+//
+//  Created by Lebedev Aleksey on 23/3/14.
+//  Copyright (c) 2014 Aleksey Lebedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AEAppDelegate class]));
+  }
+}
